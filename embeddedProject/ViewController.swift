3293fc699d5f6aacadfc09de76768ef28fdea02c
Abanoub Ghaly <abanoubghaly@Abanoubs-Mac.local>
2019-05-04 06:45:28 -0700

    Initial Commit

---
+++ embeddedProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  embeddedProject
+//
+//  Created by Abanoub Ghaly on 5/4/19.
+//  Copyright © 2019 Abanoub Ghaly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
